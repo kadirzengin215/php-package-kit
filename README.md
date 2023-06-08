434d2217cd374dfd7ca030f47b5a97523593e53f
Kadir Zengin <113590335+kadirzengin215@users.noreply.github.com>
2023-06-08 18:38:06 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# php-package-kit
+
+a simple php package starter kit
+
+[![Tests](https://github.com/kadirzengin215/php-package-kit/actions/workflows/tests.yml/badge.svg)](https://github.com/kadirzengin215/php-package-kit/actions/workflows/tests.yml)
+[![GitHub](https://img.shields.io/github/license/kadirzengin215/php-package-kit)](https://github.com/kadirzengin215/php-package-kit/blob/main/LICENSE)
---
+++ tests/Unit/ExampleTest.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * بِسْــــــــــــــــــمِ اﷲِالرَّحْمَنِ اارَّحِيم
+ *
+ * اللهم صل علي سيدنا محمد وعلى آل سيدنا محمد
+ */
+
+declare(strict_types=1);
+
+namespace KadirZengin215\Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * ExampleTest class
+ *
+ * @author Kadir Zengin <kadirzengin215@gmail.com>
+ */
+class ExampleTest extends TestCase
+{
+    /**
+     * A basic test example.
+     */
+    public function test_that_true_is_true(): void
+    {
+        $this->assertTrue(true);
+    }
+}
